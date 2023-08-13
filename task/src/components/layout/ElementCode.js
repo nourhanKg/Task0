dbe5884d4b21cd53df227c02050f265aee6201b1
Nourhan Aboelenen <Nourhan.Aboelenen@linkdev.com>
2023-08-13 16:49:51 +0300

    copy tooltip added

--- task/src/components/layout/ElementCode.js
+++ task/src/components/layout/ElementCode.js
@@ -1,8 +1,15 @@
-import { OverlayTrigger, Col } from "react-bootstrap";
+import { OverlayTrigger, Tooltip, Overlay} from "react-bootstrap";
+import { useState, useRef } from "react";
 const ElementCode = function (props) {
+  const target = useRef(null);
+  const [showOverlay, setShowOverlay] = useState(false);
+  const copyBtnHandler = function() {
+    copyCode();
+    setShowOverlay(true);
+    setTimeout(() => setShowOverlay(false), 800);
+  }
   const copyCode = function () {
     navigator.clipboard.writeText(props.code);
-    alert("Code copied")
   };
   return (
     <div className="my-4 shadow-lg bg-dark text-light">
@@ -10,12 +17,21 @@ const ElementCode = function (props) {
         <pre>
           <code>{props.code}</code>
         </pre>
-        <button
-          onClick={copyCode}
-          className="position-absolute top-0 end-0 m-2"
-        >
-          <i className="bi bi-clipboard"></i>
-        </button>
+        <span className="position-absolute top-0 end-0 m-2">
+          <button
+            ref={target}
+            onClick={copyBtnHandler}
+          >
+            <i className="bi bi-clipboard"></i>
+          </button>
+          <Overlay target={target.current} show={showOverlay} placement="top">
+            {(props) => (
+              <Tooltip id="overlay-example" {...props}>
+                Copied!
+              </Tooltip>
+            )}
+          </Overlay>
+        </span>
       </div>
     </div>
   );
--- task/src/components/layout/NavBar.js
+++ task/src/components/layout/NavBar.js
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 function NavigatinBar() {
   return (
     <Navbar expand="lg" data-bs-theme="dark" className='py-0'>
